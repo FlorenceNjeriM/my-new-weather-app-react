9d55544500f647c1faf4db2536edb453f08f13da
FlorenceNjeriM <159240271+FlorenceNjeriM@users.noreply.github.com>
2024-12-10 12:02:14 +0300

    Added a footer

--- src/Weatherr.js
+++ src/Weatherr.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Weatherr(){
     return(
         <div>
-           Weather app
+           <footer>
+            This page is coded by 
+            <a href="https://github.com/FlorenceNjeriM" >Florence Njeri</a> and hosted on <a href="https://github.com/FlorenceNjeriM/my-new-weather-app-react" > Github.</a>
+           </footer>
         </div>
     )
     
