1dd4ccf4e78acf24d3857a663efe60342452cf99
Florence Njeri Maina <159240271+FlorenceNjeriM@users.noreply.github.com>
2024-12-23 11:53:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App() {
            
            <footer class="text-center">
             This page is coded by 
-            <a href="https://github.com/FlorenceNjeriM" target="_blank" rel="noreferrer"> Florence Njeri, </a>
-    <a href="https://github.com/FlorenceNjeriM/my-new-weather-app-react" target="_blank" rel="noreferrer"> is open sourced on Github.</a> and <a href="https://app.netlify.com/sites/dulcet-dasik-29a598/overview" target="_blank" rel="noreferrer" > hosted on Netlify</a>
+            <a href="https://github.com/FlorenceNjeriM" target="_blank" rel="noreferrer"> Florence Njeri, </a> is
+    <a href="https://github.com/FlorenceNjeriM/my-new-weather-app-react" target="_blank" rel="noreferrer">  open sourced on Github.</a> and <a href="https://app.netlify.com/sites/dulcet-dasik-29a598/overview" target="_blank" rel="noreferrer" > hosted on Netlify</a>
            </footer>
         </div>
     </div>
