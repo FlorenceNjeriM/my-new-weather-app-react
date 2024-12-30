fdf106c59cbb8341c02c3c235ddac01b56ca5e99
FlorenceNjeriM <159240271+FlorenceNjeriM@users.noreply.github.com>
2024-12-30 18:02:51 +0300

    updated the weather state

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { WeatherSvg } from "weather-icons-animated";
 
-export default function WeatherIcon (props){
-    const codeMapping = {
-        "01d": "sunny",
-        "01n": "clear-night",
-        "02d": "partly-cloudy",
-        "02n": "cloudy",
-        "03d": "clouds",
-        "03n": "cloudy",
-        "04d": "cloudy",
-        "04n": "cloudy",
-        "09d": "rainy",
-        "09n": "rainy",
-        "10d": "pouring",
-        "10n": "hail",
-        "11d": "lightning",
-        "11n": "lightning-rainy",
-        "13d": "snowy",
-        "13n": "snowy-rainy",
-        "50d": "mist",
-        "50n": "fog",
-      };
-const weatherState= codeMapping[props.code]
+export default function WeatherIcon(props) {
+  const codeMapping = {
+    "01d": "sunny",
+    "01n": "clear-night",
+    "02d": "partly-cloudy",
+    "02n": "cloudy",
+    "03d": "partly-cloudy",
+    "03n": "cloudy",
+    "04d": "cloudy",
+    "04n": "cloudy",
+    "09d": "rainy",
+    "09n": "rainy",
+    "10d": "pouring",
+    "10n": "pouring",
+    "11d": "hail",
+    "11n": "hail",
+    "13d": "snowy",
+    "13n": "snowy-rainy",
+    "50d": "fog",
+    "50n": "fog",
+  };
 
-      
+  const iconState = codeMapping[props.code] || "sunny"; 
+ 
 
-        return (<WeatherSvg state= {weatherState}  width={props.width} height={props.height}/> )
-
+  return (
+    <WeatherSvg state={iconState} width={props.width} height={props.height} />
+  );
 }
